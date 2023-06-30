f045ae515cf1c81b11c4f30dd12aca30524befca
kazimahin68 <kazimahin68@gmail.com>
2023-06-30 21:06:50 +0600

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div>
+            <h2 className="text-red-600 font-extrabold text-center text-6xl">This is home page</h2>
+        </div>
+    );
+};
+
+export default Home;
